cb8ee9499646fb09f6bfbf20ac5f9671331bd48e
Vestimir Markov <me@vestimir.com>
2018-03-06 13:26:57 +0200

    Change config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  exportPathMap: () => {
-    return {
-      '/': { page: '/' },
-    }
-  }
-})
+// module.exports = withSass({
+//   exportPathMap: () => {
+//     return {
+//       '/': { page: '/' },
+//     }
+//   }
+// })
+module.exports = withSass()
